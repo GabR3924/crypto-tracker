38f2e335baa39c7fe78b5a42c293eaa07a3e5512
AleG <89297655+GabR3924@users.noreply.github.com>
2025-04-24 11:59:30 -0400

    empieza

---
+++ apps/binance_adapter/config.py
@@ -0,0 +1,9 @@
+# Constantes y configuración
+API_URL = "https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search"
+BACKEND_URL = "https://binance-dashboard-amber.vercel.app/api/marketdata"
+ASSET = "USDT"
+FIAT = "VES"
+MIN_TRANSACTION_THRESHOLD = 320
+REQUIRED_PAYMENT_METHOD = "Banesco"
+MAX_USER_GRADE = 2
+ROWS_PER_PAGE = 5
