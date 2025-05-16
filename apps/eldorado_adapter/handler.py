017aa3877c1e45069b0e432b212825c4d0ce2888
GabR3924 <garc2004@gmail.com>
2025-05-15 20:56:12 -0400

    perfecto

--- apps/eldorado_adapter/handler.py
+++ apps/eldorado_adapter/handler.py
@@ -6,17 +6,20 @@ def get_offers_with_headers(type_id, limit, amount, amount_currency_id, crypto_c
 
     # Parámeteros de la consulta
     params = {
-        'type': type_id,
-        'limit': limit,
-        'amount': amount,
-        'amountCurrencyId': amount_currency_id,
-        'sortAsc': sort_asc,
-        'cryptoCurrencyId': crypto_currency_id,
-        'fiatCurrencyId': fiat_currency_id,
-        'paymentMethods': payment_methods,
-        'showUserOffers': show_user_offers,
-        'showFavoriteMMOnly': show_favorite_mm_only
-    }
+    'type': type_id,
+    'limit': limit,
+    'amount': amount,
+    'amountCurrencyId': amount_currency_id,
+    'sortAsc': str(sort_asc).lower(),
+    'cryptoCurrencyId': crypto_currency_id,
+    'fiatCurrencyId': fiat_currency_id,
+    'paymentMethods': payment_methods,
+    'showUserOffers': str(show_user_offers).lower(),
+    'showFavoriteMMOnly': str(show_favorite_mm_only).lower(),
+    'userId': '',
+    'lastKey': ''
+}
+
     
     # Headers
     headers = {
@@ -49,20 +52,51 @@ def get_offers_with_headers(type_id, limit, amount, amount_currency_id, crypto_c
         print(f"Error: {response.status_code}")
         return None
 
-# Ejemplo de uso con los parámetros de la URL proporcionada
+def display_selected_data(offers):
+    if offers and 'data' in offers:
+        for offer in offers['data']:
+            offer_id = offer.get('offerId')
+            user_id = offer['user'].get('id') if 'user' in offer else None
+            username = offer['user'].get('username') if 'user' in offer else None
+            offer_status = offer.get('offerStatus')
+            offer_type = offer.get('offerType')
+
+            crypto_currency_id = offer.get('cryptoCurrencyId')
+            chain = offer.get('chain')
+            fiat_currency_id = offer.get('fiatCurrencyId')
+            max_limit = offer.get('maxLimit')
+            min_limit = offer.get('minLimit')
+            fiat_crypto_exchange = offer.get('fiatToCryptoExchangeRate')
+            payment_methods = offer.get('paymentMethods')
+
+            print(f"Offer ID: {offer_id}")
+            print(f"User ID: {user_id}")
+            print(f"Username: {username}")
+            print(f"Offer Status: {offer_status}")
+            print(f"Offer Type: {offer_type}")
+            print(f"Crypto Currency ID: {crypto_currency_id}")
+            print(f"Chain: {chain}")
+            print(f"Fiat Currency ID: {fiat_currency_id}")
+            print(f"Max Limit: {max_limit}")
+            print(f"Min Limit: {min_limit}")
+            print(f"fiat_crypto_exchange: {fiat_crypto_exchange}")
+            print(f"Payment Methods: {', '.join(payment_methods) if payment_methods else 'N/A'}")
+            print("-" * 30)
+    else:
+        print("No se encontraron datos relevantes.")
+
+# Ejemplo de uso con los parámetros
 type_id = 0
-limit = 3
-amount = 5
-amount_currency_id = 'TATUM-TRON-USDT'
+limit = 10
+amount = ''
+amount_currency_id = 'USD'
 crypto_currency_id = 'TATUM-TRON-USDT'
 fiat_currency_id = 'USD'
-payment_methods = 'app_zinli_us'
+payment_methods = ''
 show_user_offers = True
 show_favorite_mm_only = False
-sort_asc = False # Cambiar a 'False' según el parámetro sortAsc=false en la URL
+sort_asc = False
 
 offers = get_offers_with_headers(type_id, limit, amount, amount_currency_id, crypto_currency_id, fiat_currency_id, payment_methods, show_user_offers, show_favorite_mm_only, sort_asc)
 
-if offers:
-    # Mostrar el resultado con indentación
-    print(json.dumps(offers, indent=4))
+display_selected_data(offers)
