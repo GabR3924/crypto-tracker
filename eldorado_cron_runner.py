21b5afde5f49b8610cffdf4eedddcd5ea04ee05f
GabR3924 <garc2004@gmail.com>
2025-05-27 21:27:10 -0400

    amount bien

--- eldorado_cron_runner.py
+++ eldorado_cron_runner.py
@@ -10,8 +10,8 @@ logging.basicConfig(
 # Puedes definir diferentes combinaciones de parámetros a probar
 markets = [
     {
-        "limit": 50,
-        "amount": '',
+        "limit": 10,
+        "amount": '50',
         "amount_currency_id": "USD",
         "crypto_currency_id": "TATUM-TRON-USDT",
         "fiat_currency_id": "USD",
@@ -19,16 +19,16 @@ markets = [
         "show_user_offers": True,
         "show_favorite_mm_only": False
     },
-     {
-        "limit": 10,
-        "amount": '',
-        "amount_currency_id": "TATUM-TRON-USDT",
-        "crypto_currency_id": "TATUM-TRON-USDT",
-        "fiat_currency_id": "VES",
-        "payment_methods": 'bank_banesco',
-        "show_user_offers": True,
-        "show_favorite_mm_only": False
-    },
+    #  {
+    #     "limit": 10,
+    #     "amount": '',
+    #     "amount_currency_id": "TATUM-TRON-USDT",
+    #     "crypto_currency_id": "TATUM-TRON-USDT",
+    #     "fiat_currency_id": "VES",
+    #     "payment_methods": 'bank_banesco',
+    #     "show_user_offers": True,
+    #     "show_favorite_mm_only": False
+    # },
     # Puedes agregar más configuraciones si quieres comparar mercados
 ]
 
